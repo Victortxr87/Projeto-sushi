7ce25c569e15653475df71ac2c39a26caead2bad
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-03-26 21:14:25 +0100

    melhorando o código

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-function enviarParaWhatsApp() {
+function enviarFormulario() {
+  const nome = document.getElementById('nomeesobrenome').value;
+  const email = document.getElementById('E-mail').value;
+  const telefone = document.getElementById('Telefone').value;
+  const mensagem = document.getElementById('mensagem').value;
+  const contato = document.querySelector('input[name="contato"]:checked').value;
+  const horario = document.querySelector('select').value;
+
+  // Verifica se todos os campos estão preenchidos
+  if (nome !== "" && email !== "" && telefone !== "" && contato !== "" && horario !== "") {
+    let texto = `Olá, meu nome é ${nome}. Meu email é ${email}, meu telefone é ${telefone} e eu gostaria de ser contatado por ${contato} no horário da ${horario}. `;
     
-  const nome = document.getElementById("nomeesobrenome").value;
-  const email = document.getElementById("E-mail").value;
-  const telefone = document.getElementById("Telefone").value;
-  const mensagem = document.getElementById("mensagem").value;
-
-  
-  const tipoContato = document.querySelector('input[name="contato"]:checked').value;
-
- 
-  let texto = `Olá, meu nome é ${nome}.`;
-
-  if (tipoContato === "E-mail") {
-    texto += ` Meu e-mail é ${email}.`;
-  } else if (tipoContato === "telefone") ;
-   else {
-    texto += ` Meu número é ${telefone}.`;
+    if (mensagem) {
+      texto += ` Minha mensagem é: ${mensagem}`;
+    }
+    
+    const linkWhatsapp = `https://api.whatsapp.com/send?phone=+351931373069&text=${encodeURIComponent(texto)}`;
+    
+    window.open(linkWhatsapp, "_blank");
   }
-
-  texto += ` Minha mensagem é: ${mensagem}`;
-
- 
-  const textoCodificado = encodeURIComponent(texto);
-
- 
-  window.open(`https://wa.me/351931373069?text=${textoCodificado}`);
 }
 
 
-const formulario = document.querySelector("form");
-formulario.addEventListener("submit", function(event) {
-  event.preventDefault(); 
-  enviarParaWhatsApp();
-});
